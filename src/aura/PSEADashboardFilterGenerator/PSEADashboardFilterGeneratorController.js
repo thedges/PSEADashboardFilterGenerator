0be4ecfffee6bbf731344fd410c373115b384a0d
Troy Hedges <thedges@salesforce.com>
2018-07-02 10:06:17 -0500

    Initial load

---
+++ src/aura/PSEADashboardFilterGenerator/PSEADashboardFilterGeneratorController.js
@@ -0,0 +1,188 @@
+({
+	doInit: function (component, event, helper) {
+		console.log('doInit invoked...');
+
+		//component.set("v.filters", []);
+
+		helper.showSpinner(component);
+		helper.getSessionID(component);
+	},
+	onObjectSel: function (component, event, helper) {
+		console.log('onObjectSel invoked...');
+		console.log('  > selObjectName=' + component.get("v.selObjectName"));
+
+		if (component.get("v.selObjectName") != "") {
+			helper.showSpinner(component);
+			helper.getObjectDef(component);
+		}
+
+	},
+	onDashboardSel: function (component, event, helper) {
+		console.log('onDashboardSel invoked...');
+		var db = component.get("v.selDashboardId");
+		//console.log(JSON.stringify(db));
+		if (component.get("v.selDashboardId") != "") {
+			helper.showSpinner(component);
+			helper.getDashboard(component);
+		}
+	},
+	onDatasetSel: function (component, event, helper) {
+		console.log('onDatasetSel invoked...');
+		var db = component.get("v.selDatasetId");
+		//console.log(JSON.stringify(db));
+		if (component.get("v.selDatasetId") != "") {
+			helper.showSpinner(component);
+			helper.getDatasetDef(component);
+		}
+	},
+	addFilter: function (component, event, helper) {
+		var filters = component.get("v.filters");
+
+
+		if (component.get("v.selDashboardId") == null || component.get("v.selDashboardId") == "" ||
+			component.get("v.selObjectName") == null || component.get("v.selObjectName") == "") {
+			var toastEvent = $A.get("e.force:showToast");
+			toastEvent.setParams({
+				title: 'Warning',
+				message: 'An Einstein Dashboard and Salesfoce sObject must be chosen before filters can be created!',
+				duration: ' 5000',
+				type: 'warning',
+				mode: 'dismissible'
+			});
+			toastEvent.fire();
+		} else {
+
+			var filter = {
+				selDataSetId: null,
+				selDatasetDef: null,
+				selDatasetXMDS: null,
+				selDatasetFieldName: null,
+				selObjectName: null,
+				selObjectDef: null,
+				selFieldName: null,
+				selFieldType: "dimension",
+				selOperation: "matches",
+				selectionFlag: null
+			};
+
+			filters.push(filter);
+
+			component.set("v.filters", filters);
+		}
+
+	},
+	deleteFilter: function (component, event, helper) {
+		console.log('deleteFilter invoked...');
+		var idx = parseInt(event.getSource().get('v.title'), 10);
+		console.log('idx=' + idx);
+		var filters = component.get("v.filters");
+
+		filters.splice(idx, 1);
+		//console.log('filters=' + JSON.stringify(filters));
+		component.set("v.filters", filters);
+
+		/*
+		var newFilters = [];
+		for (var i = 0; i < filters.length; i++) {
+			if (i != idx)
+			{
+				newFilters.push(filters[i]);
+			}
+		}
+		//component.set("v.filters", []);
+		console.log('newFilters=' + JSON.stringify(newFilters));
+		component.set("v.filters", newFilters);
+		*/
+	},
+	onDatasetChange: function (component, event, helper) {
+		console.log('onDatasetChange invoked...');
+
+		var idx = parseInt(event.getSource().get('v.name'), 10);
+		console.log('idx=' + idx);
+		component.set("v.selIndex", idx);
+
+		var filters = component.get("v.filters");
+		var datasetId = filters[idx].selDatasetId;
+		//console.log(JSON.stringify(datasetId));
+		if (datasetId != "") {
+			helper.showSpinner(component);
+			helper.getDatasetDef(component);
+		}
+
+	},
+	onMeasureChange: function (component, event, helper) {
+		console.log('onMeasureChange invoked...');
+		var filters = component.get("v.filters");
+		var idx = parseInt(event.getSource().get('v.name'), 10);
+		filters[idx].selDatasetFieldName = null;
+		component.set("v.filters", filters);
+
+	},
+	copyJSON: function (component, event, helper) {
+		var filterJSON = document.getElementById("filterJSON");
+		filterJSON.select();
+		document.execCommand("copy");
+
+		var toastEvent = $A.get("e.force:showToast");
+			toastEvent.setParams({
+				title: 'Copy to Clipboard',
+				message: 'The Wave Dashboard Filter JSON has been copied to clipboard!',
+				duration: ' 500',
+				type: 'info',
+				mode: 'dismissible'
+			});
+			toastEvent.fire();
+	},
+	onFiltersChange: function (component, event, helper) {
+		console.log('onFiltersChange invoked...');
+		var filters = component.get("v.filters");
+
+		var filterJSON = document.getElementById("filterJSON");
+
+		//console.log(JSON.stringify(filters));
+		if (filters != null && filters.length > 0) {
+			var json = {};
+			json['datasets'] = {};
+			
+			for (var i = 0; i < filters.length; i++) {
+				
+				var filter = filters[i];
+				if (filter.selDatasetDef != null && filter.selDatasetDef.name != null)
+				{
+				  var datasetName = filter.selDatasetDef.name;
+				  if (json.datasets[datasetName] == null)
+				  {
+					json.datasets[datasetName] = [];
+				  }
+
+				  var fieldFilter = {};
+				  if (filter.selDatasetFieldName != null)
+				  {
+					
+					fieldFilter.fields = [filter.selDatasetFieldName];
+				  }
+
+				  if (filter.selectionFlag != null && filter.selectionFlag == true)
+				  {
+                    fieldFilter.selection = ["$" + filter.selFieldName];
+				  }
+
+				  if (filter.selFieldName != null)
+				  {
+					fieldFilter.filter = {"operator":filter.selOperation,"values":["$" + filter.selFieldName]};
+				  }
+				  json.datasets[datasetName].push(fieldFilter);
+				}
+
+			}
+
+			filterJSON.value = JSON.stringify(json);
+		}
+		else
+		{
+			if (filterJSON != null) filterJSON.value = "";
+		}
+	}
+
+
+})
---
+++ src/aura/PSEADashboardFilterGenerator/PSEADashboardFilterGeneratorHelper.js
@@ -0,0 +1,213 @@
+({
+	getDashboards : function(component) {
+		console.log('helper.getDashboards invoked...');
+        var self = this;
+
+        var action = component.get("c.getDashboardList");
+        action.setParams({ 'sessionId' : component.get("v.sessionId") });
+
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+				var resp = actionResult.getReturnValue();
+				//console.log('dashboards=' + JSON.stringify(resp));
+                component.set("v.dashboards", resp);
+        
+                self.getObjects(component);
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+    },
+    getDashboard : function(component) {
+		console.log('helper.getDashboard invoked...');
+        var self = this;
+
+        var action = component.get("c.getDashboardById");
+        action.setParams({ 'sessionId' : component.get("v.sessionId"),
+                           'dashboardId' : component.get("v.selDashboardId") });
+
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+                self.hideSpinner(component);
+				var resp = actionResult.getReturnValue();
+                component.set("v.selDashboardDef", resp);
+                component.set("v.selDatasetDef", null);
+                component.set("v.selDatasetXMDS", null);
+			    //console.log('dashboard=' + JSON.stringify(resp));
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+    },
+    getObjects : function(component) {
+		console.log('helper.getObjectDefs invoked...');
+        var self = this;
+
+        var action = component.get("c.getObjectNameList");
+        
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+                self.hideSpinner(component);
+				var resp = actionResult.getReturnValue();
+                component.set("v.objects", resp);
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+	},
+    getObjectDef : function(component) {
+		console.log('helper.getObjectDef invoked...');
+        var self = this;
+
+        var action = component.get("c.getObjectDefByName");
+        action.setParams({ 'objectName' : component.get("v.selObjectName") });
+        
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+                self.hideSpinner(component);
+                var resp = actionResult.getReturnValue();
+                //console.log('selObject=' + JSON.stringify(resp));
+                component.set("v.selObjectDef", resp);
+
+                
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+	},
+    getDatasetDef : function(component) {
+		console.log('helper.getDatasetDef invoked...');
+        var self = this;
+
+        var idx = component.get("v.selIndex");
+        var filters = component.get("v.filters");
+        var action = component.get("c.getDatasetById");
+        action.setParams({ 'sessionId' : component.get("v.sessionId"),
+                           'datasetId' : filters[idx].selDatasetId });
+        
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+                self.hideSpinner(component);
+                var idx = component.get("v.selIndex");
+                console.log("idx=" + idx);
+                var filters = component.get("v.filters");
+                //console.log("filters=" + JSON.stringify(filters));
+                var resp = actionResult.getReturnValue();
+                //console.log('selDatasetDef=' + JSON.stringify(resp));
+                filters[idx].selDatasetDef = resp;
+                component.set("v.filters", filters);
+                this.getDatasetXMDS(component);
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+	},
+    getDatasetXMDS : function(component) {
+		console.log('helper.getDatasetXMDS invoked...'); 
+        var self = this;
+
+        var idx = component.get("v.selIndex");
+        console.log("idx=" + idx);
+        var filters = component.get("v.filters");
+        //console.log("filters=" + JSON.stringify(filters));
+        var datasetDef = filters[idx].selDatasetDef;
+        //console.log("datasetDef=" + datasetDef);
+
+        var action = component.get("c.getDatasetXMDSById");
+        action.setParams({ 'sessionId' : component.get("v.sessionId"),
+                           'datasetId' : datasetDef.id,
+                           'versionId' : datasetDef.currentVersionId });
+        
+        action.setCallback(this, function(actionResult) {
+            var state = actionResult.getState();
+            if (state === "SUCCESS") {
+                console.log('helper.getDatasetXMDS response...');
+                self.hideSpinner(component);
+                var idx = component.get("v.selIndex");
+                var filters = component.get("v.filters");
+                //console.log('filters=' + JSON.stringify(filters));
+                var resp = actionResult.getReturnValue();
+                //console.log('selDatasetXMDS=' + JSON.stringify(resp));
+                filters[idx].selDatasetXMDS = resp;
+                //console.log('filters=' + JSON.stringify(filters));
+                component.set("v.filters", filters);
+            }
+            else {
+                self.hideSpinner(component);
+                self.handleErrors(actionResult.getError());
+            }
+            
+        });
+        $A.enqueueAction(action);
+	},
+    handleErrors : function(errors) {
+        let toastParams = {
+            title: "Error!",
+            message: "Unknown error", // Default error message
+            type: "error",
+            mode: "sticky"
+        };
+        // Pass the error message if any
+        if (errors && Array.isArray(errors) && errors.length > 0) {
+            toastParams.message = errors[0].message;
+        }
+        // Fire error toast
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams(toastParams);
+        toastEvent.fire();
+    },
+    showSpinner:function(component){
+        component.set("v.IsSpinner",true);
+    },
+    hideSpinner:function(component){
+        component.set("v.IsSpinner",false);
+    },
+    getSessionID:function(component){
+        console.log('onObjectSel invoked...');
+
+        var self = this;
+        var url = window.location.href;
+        var arr = url.split("/");
+        var dotarr = arr[2].split(".");
+        
+        var vfOrigin = "https://" + dotarr[0] + ".my.salesforce.com";
+        window.addEventListener("message", function(event) {
+            if (event.origin !== vfOrigin) {
+                // Not the expected origin: Reject the message!
+                return;
+            }
+            // Only handle messages we are interested in
+            if (event.data.name === "PSWaveDashboardFilterGenerator") {
+                // Handle the message
+                //console.log(event.data.payload);
+                component.set("v.sessionId", event.data.payload);
+                self.getDashboards(component);
+            }
+        }, false);
+    }
+})
